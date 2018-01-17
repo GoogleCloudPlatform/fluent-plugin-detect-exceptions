c521ea7b687a8a1b28d695b5619ef9ee06f40771
Ling Huang <lingshi@google.com>
2018-01-17 16:09:38 -0500

    Fix Rubocop warning about line too long.

--- lib/fluent/plugin/exception_detector.rb
+++ lib/fluent/plugin/exception_detector.rb
@@ -80,7 +80,8 @@ module Fluent
       rule(:go_after_panic, /^\[signal /, :go_after_signal),
       rule(:go_after_signal, /^$/, :go_goroutine),
       rule(:go_goroutine, /^goroutine \d+ \[[^\]]+\]:$/, :go_frame_1),
-      rule(:go_frame_1, /^(?:[^\s.:]+\.)*[^\s.():]+\(|^created by /, :go_frame_2),
+      rule(:go_frame_1, /^(?:[^\s.:]+\.)*[^\s.():]+\(|^created by /,
+           :go_frame_2),
       rule(:go_frame_1, /^$/, :go_goroutine),
       rule(:go_frame_2, /^\s/, :go_frame_1)
     ].freeze
