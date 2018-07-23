71718791cebc0525a48aa0f4e7ab4ed334e7d1e2
Martin Dietze <mdietze@gmail.com>
2018-07-23 18:43:39 +0800

    Teach exception_detector.rb to deal with escaped tabs (\u0009) in JSON streams of Java exceptions (#33: Escaped characters U+0000..001F in JSON not dealt with correctly).

--- lib/fluent/plugin/exception_detector.rb
+++ lib/fluent/plugin/exception_detector.rb
@@ -53,9 +53,9 @@ module Fluent
       rule(:start_state,
            /(?:Exception|Error|Throwable|V8 errors stack trace)[:\r\n]/,
            :java),
-      rule(:java, /^[\t ]+(?:eval )?at /, :java),
-      rule(:java, /^[\t ]*(?:Caused by|Suppressed):/, :java),
-      rule(:java, /^[\t ]*... \d+\ more/, :java)
+      rule(:java, /^(\\u0009|[\t ])+(?:eval )?at /, :java),
+      rule(:java, /^(\\u0009|[\t ])*(?:Caused by|Suppressed):/, :java),
+      rule(:java, /^(\\u0009|[\t ])*... \d+\ more/, :java)
     ].freeze
 
     PYTHON_RULES = [
