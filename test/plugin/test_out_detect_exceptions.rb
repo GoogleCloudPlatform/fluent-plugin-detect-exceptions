b1f57bc97ff5648014c17386d1d07cf54a965970
Alexander Faxå <alexander.faxa@gmail.com>
2020-06-18 19:33:50 +0200

    Require "remove_tag_prefix" to be specified in configs. (#78)
    Without this option specified, the output of the plugin will be fed back to
    itself, causing infinite recursion and errors that can be hard to understand.

--- test/plugin/test_out_detect_exceptions.rb
+++ test/plugin/test_out_detect_exceptions.rb
@@ -28,6 +28,8 @@ END
 
   DEFAULT_TAG = 'prefix.test.tag'.freeze
 
+  DEFAULT_TAG_STRIPPED = 'test.tag'.freeze
+
   ARBITRARY_TEXT = 'This line is not an exception.'.freeze
 
   JAVA_EXC = <<END.freeze
@@ -125,7 +127,9 @@ END
     }
 
     test_cases.each do |language, exception|
-      cfg = "languages #{language}"
+      cfg = %(
+#{CONFIG}
+languages #{language})
       d = create_driver(cfg)
       t = Time.now.to_i
 
@@ -156,12 +160,12 @@ END
 
       # Validate that each line received is emitted separately as expected.
       router_mock.should_receive(:emit)
-                 .once.with(DEFAULT_TAG, Integer,
+                 .once.with(DEFAULT_TAG_STRIPPED, Integer,
                             'message' => json_line_with_exception,
                             'count' => 0)
 
       router_mock.should_receive(:emit)
-                 .once.with(DEFAULT_TAG, Integer,
+                 .once.with(DEFAULT_TAG_STRIPPED, Integer,
                             'message' => json_line_without_exception,
                             'count' => 1)
 
@@ -174,7 +178,9 @@ END
   end
 
   def test_single_language_config
-    cfg = 'languages java'
+    cfg = %(
+#{CONFIG}
+languages java)
     d = create_driver(cfg)
     t = Time.now.to_i
     d.run do
@@ -185,7 +191,9 @@ END
   end
 
   def test_multi_language_config
-    cfg = 'languages python, java'
+    cfg = %(
+#{CONFIG}
+languages python, java)
     d = create_driver(cfg)
     t = Time.now.to_i
     d.run do
@@ -196,7 +204,9 @@ END
   end
 
   def test_split_exception_after_timeout
-    cfg = 'multiline_flush_interval 1'
+    cfg = %(
+#{CONFIG}
+multiline_flush_interval 1)
     d = create_driver(cfg)
     t1 = 0
     t2 = 0
@@ -227,6 +237,12 @@ END
     assert_equal(make_logs(t1, JAVA_EXC + "  at x\n  at y\n"), d.events)
   end
 
+  def test_remove_tag_prefix_is_required
+    cfg = ''
+    e = assert_raises(Fluent::ConfigError) { create_driver(cfg) }
+    assert_match(/remove_tag_prefix/, e.message)
+  end
+
   def get_out_tags(remove_tag_prefix, original_tag)
     cfg = "remove_tag_prefix #{remove_tag_prefix}"
     d = create_driver(cfg, original_tag)
@@ -244,7 +260,9 @@ END
   end
 
   def test_flush_after_max_lines
-    cfg = 'max_lines 2'
+    cfg = %(
+#{CONFIG}
+max_lines 2)
     d = create_driver(cfg)
     t = Time.now.to_i
     d.run do
@@ -263,7 +281,9 @@ END
   end
 
   def test_separate_streams
-    cfg = 'stream stream'
+    cfg = %(
+#{CONFIG}
+stream stream)
     d = create_driver(cfg)
     t = Time.now.to_i
     d.run do
